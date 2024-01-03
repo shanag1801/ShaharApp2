7a44edd4c86aca7eff478c4dbb795e2190bed1d6
shanag1801 <shanag0110@gmail.com>
2024-01-03 08:33:40 +0200

    Added a go function

--- app/src/main/java/com/example/shaharapp2/MainActivity.java
+++ app/src/main/java/com/example/shaharapp2/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.shaharapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    int count;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+        count = 0;
+    }
+    @SuppressLint("SetTextI18n")
+    public void go(View view){
+        count ++;
+        btn.setText("This is a click number: " + count);
     }
 }
